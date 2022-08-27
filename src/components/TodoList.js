c06d4094db353ad0db7a604901aeb5805b9f2bd4
Sofya Nagaslaeva <sofyanagaslaeva@gmail.com>
2022-08-27 15:41:47 -0700

    add edit and delete functionality

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -7,11 +7,22 @@
 
    const addTodo = todo => {
     if(!todo.text || /^\s*$/.test(todo.text)){
-        return
+        return;
     }
-
     const newTodos = [todo, ...todos];
     setTodos(newTodos);
+   }
+
+   const updateTodo = (todoId, newValue) => {
+    if(!newValue.text || /^\s*$/.test(newValue.text)){
+        return;
+    }
+    setTodos(prev => prev.map(item => (item.id === todoId? newValue : item)))
+   }
+
+   const removeTodo = id => {
+    const removeArr = [...todos].filter(todo => todo.id !==id);
+    setTodos(removeArr);
    }
 
    const completeTodo = id => {
@@ -28,7 +39,9 @@
         <div>
             <h1>What's the Plan for Today?</h1>
             <TodoForm onSubmit={addTodo} />
-            <Todo todos={todos} completeTodo={completeTodo}/>
+            <Todo todos={todos} completeTodo={completeTodo}
+            removeTodo={removeTodo}
+            updateTodo={updateTodo}/>
         </div>
    )
  }
