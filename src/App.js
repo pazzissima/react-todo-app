ab9da8b347db5d6f4508876720f08fd4a2e58602
Sofya Nagaslaeva <sofyanagaslaeva@gmail.com>
2022-08-27 14:58:27 -0700

    created and connected 3 components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import TodoList from './components/TodoList';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Todo app</h1>
+    <div className="todo-app">
+      <TodoList />
     </div>
   );
 }
